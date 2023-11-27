8535f0400b366b707991232e05c09a8b3acf602e
Christine-design-web <wakuthiichristine5@gmail.com>
2023-11-27 22:54:10 +0300

    new codes

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,12 @@
 export default function createIteratorObject(report) {
-  while (report) {
-    console.log('Placeholder');
-  }
+  return {
+    [Symbol.iterator]: function* () {
+      for (const department in report.allEmployees) {
+        const employeesInDepartment = report.allEmployees[department];
+        for (const employee of employeesInDepartment) {
+          yield employee;
+        }
+      }
+    },
+  };
 }
