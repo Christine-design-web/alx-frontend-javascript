f8a3829d7bf646b6dc0d56427742444d5de5ec95
Christine-design-web <wakuthiichristine5@gmail.com>
2023-12-01 05:02:33 +0300

    fixed the error

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -28,9 +28,14 @@ class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this.holbertonClass.year} - ${this.holbertonClass.location}`;
+ }
 }
+
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
